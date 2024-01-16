45fe186a1f7f7ff4fa6de2b1264d1afb4d6daf74
AnnyTerfect <1247470246@qq.com>
2024-01-16 16:23:07 +0800

    fix: fix router path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const { resolve } = require("path");
+
 const nextConfig = {
   output: "export",
+  webpack(config) {
+    config.resolve.modules.push(resolve("./"));
+    return config;
+  },
   basePath: "/zhengqc",
+  assetPrefix: "/zhengqc",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
